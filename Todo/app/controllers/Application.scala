5d64652c7bfb52c83e03ffc05aa2d1303f0d50ff
Aakash Patel <aakpat6@gmail.com>
2013-10-06 23:57:57 -0400

    Basic todo list functionality.

--- Todo/app/controllers/Application.scala
+++ Todo/app/controllers/Application.scala
@@ -1,12 +1,31 @@
 package controllers
 
-import play.api._
-import play.api.mvc._
+import play.api.data.Form
+import play.api.data.Forms
+import play.api.mvc.Action
+import play.api.mvc.Controller
+import models.Task
 
 object Application extends Controller {
 
+  val taskForm = Form("label" -> Forms.nonEmptyText)
+
   def index = Action {
-    Ok(views.html.main("Hello, World!"))
+    Ok(views.html.index(Task.all(), taskForm))
   }
 
+  def newTask = Action { implicit request =>
+    taskForm.bindFromRequest fold (
+      errors => BadRequest(views.html.index(Task all (), errors)),
+      label => {
+        Task create label
+        Redirect(routes.Application.index)
+      })
+  }
+
+  def deleteTask(id: Long) = Action {
+    println(id)
+    Task delete id
+    Redirect(routes.Application.index)
+  }
 }
